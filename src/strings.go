bbc18d8dd6b1c85b062432cac3bc03785de13911
Administrator <support@e154.ru>
2014-11-30 23:43:37 +0600

    strings

--- src/strings.go
+++ src/strings.go
@@ -1,10 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	str := "xyz"
 	str2 := ""
-	fmt.Printf("%d %d\n",len(str), len(str2))
+	bool := true
+	// len - количество байт в строке
+	fmt.Printf("Количество байт в строках: %d %d\n",len(str), len(str2))
 
+	// количество символов в строке
+	count := utf8.RuneCountInString(str)
+	fmt.Printf("Количество символов в строке %q: %d\n", str, count)
+
+	fmt.Printf("bool: %t\n", bool)
+
+	p := fasle
+	fmt.Printf("|%T|%v|%#v|\n", p, p, p)
 }
