0d2426d3928cfcd41eed172d708e561b863b47a3
delta54 <support@e154.ru>
2015-05-11 15:12:35 +0600

    get last element

--- src/url.go
+++ src/url.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/url"
+	"strings"
 )
 
 var (
@@ -11,14 +12,17 @@ var (
 )
 
 func main() {
-	fmt.Printf("url\n")
 
 	params := url.Values{}
 	params.Add("brand", "ABARTH")
 	params.Add("model", "500")
 	params.Add("version", "500 Abarth 2008->0")
 	params.Add("publication", "2151")
-	baseUrl, _ := url.Parse("http://www.goodyear.eu/tirefinder/api/by_car/")
+	baseUrl, _ := url.Parse("http://www.goodyear.eu/tirefinder/api/by_car/image.jpg")
 	baseUrl.RawQuery = params.Encode()
 	fmt.Println(baseUrl.String())
+
+	// last element
+	p := strings.Split(baseUrl.Path, "/")
+	fmt.Println(p[len(p)-1])
 }
