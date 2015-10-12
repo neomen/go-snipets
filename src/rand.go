0e509c9b29e53d8474753b19cfda004b1cbf714b
delta54 <support@e154.ru>
2015-10-12 22:38:30 +0600

    random update

--- src/rand.go
+++ src/rand.go
@@ -3,7 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
+
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max - min) + min
+}
 
 func main() {
 	fmt.Printf("Math rand num = %d\n", rand.Int())
@@ -11,4 +17,7 @@ func main() {
 	for i := 0; i<10; i++ {
 		fmt.Println(rand.Int())
 	}
+
+	myrand := random(1, 99)
+	fmt.Println(myrand)
 }
