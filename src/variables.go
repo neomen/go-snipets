48754c2ca44340a24b27658fe4fc512525bf4789
delta54 <support@e154.ru>
2015-10-13 23:20:28 +0600

    variables update

--- src/variables.go
+++ src/variables.go
@@ -5,6 +5,13 @@ import "fmt"
 var i int
 var c, python, java = true, false, "no!"
 
+var (
+	array = [...]string{
+		"a",
+		"b",
+	}
+)
+
 func main() {
 	k := 3
 	fmt.Println(i, c, python, java, k)
