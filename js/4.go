5bdd5f846335d1cec66cebaea164a6b7f239380d
praetorian <filippov.a@e154.ru>
2017-11-03 02:05:10 +0700

    js

---
+++ js/4.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/e154/go-candyjs"
+	"fmt"
+)
+
+const script2 =  `
+var a = "a";
+`
+
+const script3 =  `
+var b = 23;		`
+
+const script4 =  `
+
+var main;
+
+main = function() {
+	print('main', a, b);
+	return -1;
+};
+
+main();
+`
+
+func main() {
+	ctx := candyjs.NewContext()
+
+	//ctx.EvalString(script3)
+	//ctx.EvalString(script4)
+
+	if err := ctx.PevalString(script2); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err := ctx.PevalString(script3); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err := ctx.PevalString(script4); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+
+	ctx.PushTimers()
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Script: Recovered in f", r)
+		}
+	}()
+
+	result := ctx.SafeToString(-1)
+	fmt.Println("result:",result)
+}
